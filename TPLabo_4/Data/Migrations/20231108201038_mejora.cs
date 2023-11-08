e9e54ef58928a6225f46d178bd0d98cdebe5e2ec
Leonel Armando <leonellaspiur@gmail.com>
2023-11-08 17:59:13 -0300

    actualizacion

---
+++ TPLabo_4/Data/Migrations/20231108201038_mejora.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TPLabo_4.Data.Migrations
+{
+    public partial class mejora : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
